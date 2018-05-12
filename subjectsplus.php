85d08627ccfec2d88d44c348052ebf2465537078
rho <rho.rhoit@gmail.com>
2018-05-12 13:03:14 +0545

    cleaning up little shit code
    - fix indentation
    - remove trailing white space

--- subjectsplus.php
+++ subjectsplus.php
@@ -1,46 +1,42 @@
 <?php
-/*
-  Plugin Name: Subjectsplus Shortcodes
-  Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-  Description: A demonstration of the subjectsplus api using Wordpress shortcodes
-  Version: 0.1
-  Author: James Little
-  License: 
-*/
+  /*
+    Plugin Name: Subjectsplus Shortcodes
+    Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+    Description: A demonstration of the subjectsplus api using Wordpress shortcodes
+    Version: 0.1
+    Author: James Little
+    License:
+  */
 
 
 include('subjectsplusclass.php');
 
-function get_sp( $atts ){
+function get_sp($atts) {
+    extract(
+        shortcode_atts(
+            array(
+                // Misc attributes
+                'service' => '',
+                'display' => 'plain',
+                'max' => '',
+                //Staff attributes
+                'email' => '',
+                'department' => '',
+                'limit' => '99',
 
-  extract( shortcode_atts( array(
-				 // Misc attributes
-				 'service' => '',
-				 'display' => 'plain',
-				 'max' => '',
-				 //Staff attributes 
-				 'email' => '',
-				 'department' => '', 
-				 'limit' => '99',
+                //Database attributes
+                'letter' => '',
+                'limit' => '99',
+                'search' => '',
+                'subject_id' => '',
+                'type' => ''),
+            $atts));
 
-				 //Database attributes
-				 'letter' => '',
-				 'limit' => '99',
-				 'search' => '',
-				 'subject_id' => '',
-				 'type' => ''
-				 ), $atts ) );
-
-
-
-  $subjectsplus = new subjectsplus_info();
-  $subjectsplus->set_sp_url("http://subjectsplus.com/spum/api/");
-  $subjectsplus->set_sp_key("");
-  $subjectsplus->setup_sp_query($atts);
-
+    $subjectsplus = new subjectsplus_info();
+    $subjectsplus->set_sp_url("http://web:7070/api/");
+    $subjectsplus->set_sp_key("18Oc4qyugxyykBwhDlCl");
+    $subjectsplus->setup_sp_query($atts);
 }
 
 add_shortcode( 'sp', 'get_sp' );
 ?>
-
-
--- subjectsplusclass.php
+++ subjectsplusclass.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 class subjectsplus_info {
 
 	private $sp_key;
 	private $sp_url;
 	private $sp_output_xml;
-	private $sp_staff; 
+	private $sp_staff;
 	private $sp_query;
 
 	// Getter and setter for the API key
@@ -42,107 +42,102 @@ class subjectsplus_info {
 
 		$query = $this->sp_url . $this->sp_query . $this->sp_key;
 
-		// Send a get request to the SP API with the wordpress remote function 
+		// Send a get request to the SP API with the wordpress remote function
 
 		$response = wp_remote_get( $query );
-			if( is_wp_error( $response ) ) {
-   				$error_message = $response->get_error_message();
-   				echo "Error: $error_message";
-			} else {
+        if( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            echo "Error: $error_message";
+        } else {
+            $staff_info = json_decode($response[body], true);
 
-
-			   $staff_info = json_decode($response[body], true);
-
-			   if($sp_display == 'plain') {
-			   foreach ($staff_info['staff-member'] as $staff) {
+            if($sp_display == 'plain') {
+                foreach ($staff_info['staff-member'] as $staff) {
 
 			   		echo p_print($staff['fname'] . ' '  . $staff['lname']);
 			   		echo p_print($staff['title']);
 			   		echo p_print($staff['tel']);
 			   		echo p_print($staff['email']);
-			   	//	echo $staff['bio'];
-			   		
-
-			   }
+                    //	echo $staff['bio'];
+                }
 
 			}
-
 
 			if($sp_display == 'table') {
 				echo '<table width="98%" class="item_listing" cellspacing="0" cellpadding="3">
 				<tbody><tr><th>First Name</th><th>Last Name</th><th>Title</th><th>Phone</th><th>Email</th><th>Bio</th></tr>';
 
-			  foreach ($staff_info['staff-member'] as $staff) {
+                foreach ($staff_info['staff-member'] as $staff) {
 			  		echo "<tr>";
 			   		echo td($staff['fname']);
 			   		echo td($staff['lname']);
 			   		echo td($staff['title']);
 			   		echo td($staff['tel']);
 			   		echo td($staff['email']);
-			   	//	echo td($staff['bio']);
+                    //	echo td($staff['bio']);
 			   		echo "</tr>";
-			   }
-			   echo '</tbody></table>';
+                }
+                echo '</tbody></table>';
 
 
 			}
 
-			}
+        }
 
 	}
 
-	// Function to perform a database service query 
+	// Function to perform a database service query
 
 	public function do_sp_database_query() {
 
 		$query = $this->sp_url . $this->sp_query . $this->sp_key;
 
 		$response = wp_remote_get( $query );
-			if( is_wp_error( $response ) ) {
-   				$error_message = $response->get_error_message();
-   				echo "Something went wrong: $error_message";
-			} else {
+        if( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            echo "Something went wrong: $error_message";
+        } else {
 
 
-			   $database_info = json_decode($response[body], true);
+            $database_info = json_decode($response[body], true);
 
 
-			   foreach ($database_info['database'] as $database) {
-			   		echo a_link($database['location'], $database['title']);
-			   		echo p_print($database['description']);
-			   		
-			   		
-			   	
-			   }
+            foreach ($database_info['database'] as $database) {
+                echo a_link($database['location'], $database['title']);
+                echo p_print($database['description']);
 
 
-			}
+
+            }
+
+
+        }
 	}
 
 	public function do_sp_guide_query() {
 
 		$query = $this->sp_url . $this->sp_query . $this->sp_key;
 
-	
+
 		$response = wp_remote_get( $query );
-			if( is_wp_error( $response ) ) {
-   				$error_message = $response->get_error_message();
-   				echo "Something went wrong: $error_message";
+        if( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            echo "Something went wrong: $error_message";
 
-			} else {
-				  
-			  $guide_info = json_decode($response[body], true);
-				
-			   foreach ($guide_info['guide'] as $guide) {
-			   
-			     echo a_link($guide['url'],$guide['title']);
-      			   			     
-			   	
-			   }
+        } else {
+
+            $guide_info = json_decode($response[body], true);
+
+            foreach ($guide_info['guide'] as $guide) {
+
+                echo a_link($guide['url'],$guide['title']);
 
 
-			}
-	
+            }
+
+
+        }
+
 	}
 
 	// This function determines what kind of query to make based on shortcode input.
@@ -157,91 +152,86 @@ class subjectsplus_info {
 		}
 
 		switch($sp_type) {
-			case 'staff':
-
-				if (array_key_exists('email', $atts)) {
-					$sp_email = sanitize_string($atts['email']);
-					$sp_max = sanitize_string($atts['max']);
-
-					$this->sp_query = "$sp_type/email/$sp_email/max/$sp_max/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+        case 'staff':
+            if (array_key_exists('email', $atts)) {
+                $sp_email = sanitize_string($atts['email']);
+                $sp_max = sanitize_string($atts['max']);
+                $this->sp_query = "$sp_type/email/$sp_email/max/$sp_max/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_staff_query($sp_display);
-				}
+            }
 
 
-				if (array_key_exists('department', $atts)) {
-					$sp_department = sanitize_string($atts['department']);
-
-					$this->sp_query = "$sp_type/department/$sp_department/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+            if (array_key_exists('department', $atts)) {
+                $sp_department = sanitize_string($atts['department']);
+                $this->sp_query = "$sp_type/department/$sp_department/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_staff_query($sp_display);
-				}
+            }
 			break;
 
-			case 'database':
+        case 'database':
 
-				if (array_key_exists('letter', $atts)) {
-					$this->sp_query = "$sp_type/letter/$atts[letter]/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+            if (array_key_exists('letter', $atts)) {
+                $this->sp_query = "$sp_type/letter/$atts[letter]/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 
 				return $this->do_sp_database_query($sp_display);
-				}
+            }
 
-				if (array_key_exists('search', $atts)) {
-					$sp_search = sanitize_string($atts[search]);
+            if (array_key_exists('search', $atts)) {
+                $sp_search = sanitize_string($atts[search]);
 
-					$this->sp_query = "$sp_type/search/$sp_search/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+                $this->sp_query = "$sp_type/search/$sp_search/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_database_query($sp_display);
-				}
+            }
 
-				if (array_key_exists('subject_id', $atts)) {
-					$this->sp_query = "$sp_type/subject_id/$atts[subject_id]/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+            if (array_key_exists('subject_id', $atts)) {
+                $this->sp_query = "$sp_type/subject_id/$atts[subject_id]/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_database_query($sp_display);
-				}
+            }
 
-				if (array_key_exists('type', $atts)) {
-					$this->sp_query = "$sp_type/type/$atts[type]/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+            if (array_key_exists('type', $atts)) {
+                $this->sp_query = "$sp_type/type/$atts[type]/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_database_query($sp_display);
-				}
+            }
 			break;
 
-			case 'guides':
+        case 'guides':
 
-				if (array_key_exists('shortform', $atts)) {
-					$sp_search = sanitize_string($atts[search]);
+            if (array_key_exists('shortform', $atts)) {
+                $sp_search = sanitize_string($atts[search]);
 
-					$this->sp_query = "$sp_type/shortform/$sp_search/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+                $this->sp_query = "$sp_type/shortform/$sp_search/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_guide_query($sp_display);
-				}
+            }
 
-				if (array_key_exists('subject_id', $atts)) {
-					$this->sp_query = "$sp_type/subject_id/$atts[subject_id]/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+            if (array_key_exists('subject_id', $atts)) {
+                $this->sp_query = "$sp_type/subject_id/$atts[subject_id]/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_guide_query($sp_display);
-				}
+            }
 
-				if (array_key_exists('type', $atts)) {
-					$this->sp_query = "$sp_type/type/$atts[type]/max/$atts[max]/";
-					$query = $this->sp_url . $this->sp_query . $this->sp_key;
+            if (array_key_exists('type', $atts)) {
+                $this->sp_query = "$sp_type/type/$atts[type]/max/$atts[max]/";
+                $query = $this->sp_url . $this->sp_query . $this->sp_key;
 				return $this->do_sp_guide_query($sp_display);
-				}
+            }
 			break;
 
 		}
 	}
-
-
 }
 
 
 
 
 
-// Some functions to make html easier to work with in the code 
+// Some functions to make html easier to work with in the code
 
 function p_print($content) {
 
